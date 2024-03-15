0335c31488844d020da9c65a97a8474009875aa5
hareesh77 <hareesh77>
2024-03-15 11:01:00 +0000

    commit & push

---
+++ set-up/6.explore_dbfs_root.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Explore DBFS root
+# MAGIC 1.List all the folders in DBFS root\
+# MAGIC 2.Interact with DBFS File Browser\
+# MAGIC 3.upload file to DBFS Root
+
+# COMMAND ----------
+
+display(dbutils.fs.ls('/FileStore/'))
+
+# COMMAND ----------
+
+display(spark.read.csv('dbfs:/FileStore/circuits.csv'))
+
+# COMMAND ----------
+
+
